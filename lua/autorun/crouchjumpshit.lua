83e9ceeefe259c683f7c858398bf9ed2cb0017ad
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-08-03 06:13:05 +0300

    Reroll 2

--- lua/autorun/crouchjumpshit.lua
+++ lua/autorun/crouchjumpshit.lua
@@ -1,29 +1,29 @@
-if CLIENT then return end
+AddCSLuaFile()
 
-hook.Add("Think", "Crouchjumpshit", function()
-    for k,v in pairs(player.GetAll()) do
-        if !v:OnGround() && v:IsFlagSet(6) then
-            local nextpos = (v:GetVelocity() + physenv.GetGravity()) * engine.TickInterval()
-            local pos = v:GetPos() + nextpos
-            local smins, smaxs = v:GetHull()
-            local cmins, cmaxs = v:GetHullDuck()
+hook.Add("FinishMove", "Crouchjumpshit", function(ply, mv)
+    -- for k,v in pairs(player.GetAll()) do
+        if !ply:OnGround() && ply:IsFlagSet(6) then
+            local nextpos = (ply:GetVelocity() + physenv.GetGravity()) * engine.TickInterval()
+            local pos = ply:GetPos() + nextpos
+            local smins, smaxs = ply:GetHull()
+            local cmins, cmaxs = ply:GetHullDuck()
             local trh = util.TraceHull({
             start = pos,
             endpos = pos - Vector(0,0,smaxs.z),
             mins = cmins,
             maxs = cmaxs,
             mask = 33636363,
-            filter = v})
+            filter = ply})
             local trl = util.TraceLine({
             start = pos,
             endpos = pos - Vector(0,0,smaxs.z),
             mask = 33636363,
-            filter = v})
+            filter = ply})
             print(trh.Fraction, trl.Fraction)
-            local total = math.Clamp(trh.Fraction + trl.Fraction, 0, 2) * 0.5 * -(smaxs.z-cmaxs.z)
-            v:ManipulateBonePosition(0, Vector(0,0,total), true)
+            local total = Lerp((trh.Fraction + trl.Fraction) * 0.5, 0, -cmaxs.z)
+            ply:ManipulateBonePosition(0, Vector(0,0,total), false)
         else
-            v:ManipulateBonePosition(0, Vector(0,0,0), true)
+            ply:ManipulateBonePosition(0, Vector(0,0,0), false)
         end
-    end
+    -- end
 end)

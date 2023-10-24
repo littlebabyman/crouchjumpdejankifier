7806e3f45a4a2cc593611956e358036830139627
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-10-24 19:26:38 +0300

    Update crouchjumpshit.lua

--- lua/autorun/crouchjumpshit.lua
+++ lua/autorun/crouchjumpshit.lua
@@ -1,28 +1,35 @@
 AddCSLuaFile()
 
 local sp = game.SinglePlayer()
-hook.Add("FinishMove", "Crouchjumpshit", function(ply, mv)
-    if !ply:OnGround() && ply:IsFlagSet(4) then
-        local nextpos = (ply:GetVelocity() + physenv.GetGravity()) * engine.TickInterval()
-        local pos = ply:GetPos() + nextpos
-        local smins, smaxs = ply:GetHull()
-        local cmins, cmaxs = ply:GetHullDuck()
-        local trh = util.TraceHull({
-        start = pos,
-        endpos = pos - Vector(0,0,smaxs.z),
-        mins = cmins,
-        maxs = cmaxs,
-        mask = 33636363,
-        filter = ply})
-        local trl = util.TraceLine({
-        start = pos,
-        endpos = pos - Vector(0,0,smaxs.z),
-        mask = 33636363,
-        filter = ply})
+hook.Add("SetupMove", "Crouchjumpshit", function(ply, mv, cmd)
+    if CLIENT && !IsFirstTimePredicted() then return end
+    if !ply:OnGround() && ply:IsFlagSet(2) then
+        local nextpos = (mv:GetVelocity()) * FrameTime()
+        local pos = ply:GetPos()
+        -- local smins, smaxs = ply:GetHull()
+        -- local cmins, cmaxs = ply:GetHullDuck()
+        local tr = {}
+        tr.start = pos
+        tr.endpos = pos + Vector(0,0,-36)
+        tr.mask = MASK_SOLID
+        tr.filter = ply
+        local trh = util.TraceEntity(tr, ply)
+        -- local trl = util.TraceLine(tr)
         -- print(trh.Fraction, trl.Fraction)
-        local total = Lerp((trh.Fraction + trl.Fraction) * 0.5, 0, -cmaxs.z)
+        local total = Lerp(trh.Fraction, 0, -36)
         ply:ManipulateBonePosition(0, Vector(0,0,total), sp)
+        -- ply:ManipulateBonePosition(0, Vector(0,0,total), sp)
     else
         ply:ManipulateBonePosition(0, Vector(0,0,0), sp)
     end
 end)
+
+if SERVER then
+    hook.Add("DoPlayerDeath", "GiveAmmoOnKill", function(killed, ply, dmginfo)
+        if !IsValid(ply) || !ply:IsPlayer() || ply == killed then return end
+        local at, wep = dmginfo:GetAmmoType(), dmginfo:GetInflictor()
+        if at == -1 then return end
+        local ammo = math.max(wep:IsWeapon() && wep:GetMaxClip1() || ply:GetActiveWeapon():GetMaxClip1(), 1)
+        ply:GiveAmmo(ammo, at, true)
+    end)
+end
